1d2843821e9c4fdc21f27de9f9a02c294fcc95e3
jmpfbmx <jmpf.bmx@gmail.com>
2020-05-10 01:37:29 +0200

    olive: Import sensors

--- device.mk
+++ device.mk
@@ -197,6 +197,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 #Securelock
 PRODUCT_COPY_FILES += \
     device/xiaomi/olive/configs/securelock.xml:system/etc/permissions/securelock.xml
