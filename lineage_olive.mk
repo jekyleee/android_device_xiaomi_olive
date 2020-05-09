2e16cf9f8bed109e4b556bf1f2a5fdac68c2dfdf
jmpfbmx <jmpf.bmx@gmail.com>
2020-05-10 00:08:39 +0200

    olive: pine -> olive

--- lineage_olive.mk
+++ lineage_olive.mk
@@ -21,18 +21,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from the common LineageOS configuration.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from pine device
-$(call inherit-product, device/xiaomi/pine/device.mk)
+# Inherit from olive device
+$(call inherit-product, device/xiaomi/olive/device.mk)
 
-PRODUCT_DEVICE := pine
-PRODUCT_NAME := lineage_pine
+PRODUCT_DEVICE := olive
+PRODUCT_NAME := lineage_olive
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 7A
+PRODUCT_MODEL := Redmi 8A
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pine-user 9 PKQ1.190319.001 V10.2.5.0.PCMMIXM release-keys" \
-    PRODUCT_NAME="pine" \
-    TARGET_DEVICE="pine"
+    PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNINXM release-keys" \
+    PRODUCT_NAME="olive" \
+    TARGET_DEVICE="olive"
 
-BUILD_FINGERPRINT := Xiaomi/pine/pine:9/PKQ1.190319.001/V10.2.5.0.PCMMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/olive/olive:9/PKQ1.190319.001/V11.0.1.0.PCNINXM:user/release-keys
