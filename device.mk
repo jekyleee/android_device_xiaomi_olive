4822832febfbecc1a3b392543214ba909c248fba
jmpfbmx <jmpf.bmx@gmail.com>
2020-05-10 02:04:44 +0200

    olive: device.mk: Add camera hals

--- device.mk
+++ device.mk
@@ -65,6 +65,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
