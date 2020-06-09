e414b37460aa4058ade407cc6beab5db39d8550d
EngMoPro <fightstart20000@yahoo.com>
2020-06-09 14:53:19 +0200

    olive:add display hal

--- device.mk
+++ device.mk
@@ -14,6 +14,28 @@
 # limitations under the License.
 #
 
+
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    vendor.display.config@1.8 \
+    libvulkan \
+    gralloc.msm8937 \
+    hwcomposer.msm8937 \
+    memtrack.msm8937 \
+    libdisplayconfig \
+    libqdMetaData.system \
+    libqdMetaData.system \
+    libtinyxml
+
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
