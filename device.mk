0728ab97abeb890e204607fff0f35a7093e02a22
EngMoPro <fightstart20000@yahoo.com>
2020-06-09 15:26:19 +0200

    olive: sensors

--- device.mk
+++ device.mk
@@ -15,6 +15,29 @@
 #
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+
+
 # GPS
 PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
