bddf43c0a4e61a4c5667127f82601f438c060720
EngMoPro <fightstart20000@yahoo.com>
2020-06-09 14:54:04 +0200

    olive: add 2nd arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,11 +57,19 @@ TARGET_FS_CONFIG_GEN := device/xiaomi/olive/config.fs
 TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
 
 # Architecture
-TARGET_ARCH := arm
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Board
 TARGET_NO_BOOTLOADER := true
