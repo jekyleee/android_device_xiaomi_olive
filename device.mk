c2a1619b9c63a37c6910ffbe3e119bd2c64a529e
Pro Logic <fightstart30000@yahoo.com>
2020-06-20 14:42:45 +0200

    rootdir: add init.target.rc start using ramdisk for system

--- device.mk
+++ device.mk
@@ -200,33 +200,34 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service
 
 # Ramdisk
-#PRODUCT_PACKAGES += \
-    #init.class_late.sh \
-    #init.class_main.sh \
-    #init.crda.sh \
-    #init.mdm.sh \
-    #init.qcom.class_core.sh \
-    #init.qcom.coex.sh \
-    #init.qcom.crashdata.sh \
-    #init.qcom.early_boot.sh \
-    #init.qcom.efs.sync.sh \
-    #init.qcom.post_boot.sh \
-    #init.qcom.sdio.sh \
-    #init.qcom.sensors.sh \
-    #init.qcom.sh \
-    #init.qcom.usb.sh \
-    #init.qti.can.sh \
-    #init.qti.charger.sh \
-    #init.qti.fm.sh \
-    #init.qti.ims.sh \
-    #init.qti.manifest_sku.sh \
-    #init.qti.qseecomd.sh \
-    #qca6234-service.sh \
-    #init.msm.usb.configfs.rc \
-    #init.qcom.factory.rc \
-    #init.qcom.rc \
-    #init.qcom.usb.rc \
-    #ueventd.qcom.rc \
+PRODUCT_PACKAGES += \
+    init.class_late.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.can.sh \
+    init.qti.charger.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    init.qti.manifest_sku.sh \
+    init.qti.qseecomd.sh \
+    qca6234-service.sh \
+    init.msm.usb.configfs.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    ueventd.qcom.rc \
+    init.target.rc
 
 
 
